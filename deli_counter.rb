e1092cdebed46fb1fb496db6ada4e8d920947fb5
Thach Luu <luuphanbaothach@gmail.com>
2020-03-17 20:39:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(arr)
+  if arr.length >= 1 
+    new_arr = []
+    
+    arr.each_with_index do |name,i|
+      new_arr << "#{i+1}. #{name}"
+      
+    end 
+    puts "The line is currently: #{new_arr.join(" ")}"
+  else 
+    puts "The line is currently empty."
+  end 
+end 
+def take_a_number(line,new_person)
+  line << new_person
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end 
+
+def now_serving(line)
+  if line.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end 
+end 
