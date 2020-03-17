53bc0704450bf1f16cbdfb8fb4d781f775523c56
Thach Luu <luuphanbaothach@gmail.com>
2020-03-17 21:14:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,12 @@ def line(arr)
     puts "The line is currently empty."
   end 
 end
+line = [] 
 def number(line,name)
+  
   line << name 
   puts "Hello, your new number is #{line.length}"
-  
+end
 def take_a_number(line,new_person)
   line << new_person
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
